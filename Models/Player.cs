f1973abe68158b55871b27846c5a1518a9a7a96b
yasum <yasum.abbas137@gmail.com>
2022-07-01 12:22:47 +0100

    Added Player class

---
+++ Models/Player.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEEK7.Models
+    }
+    public class Player {
+        public string playerName { get; set; }
+        public ArrayList<String> listOfCards { get; set}
+        public int score { get; set; }
+        public bool goingFirst { get; set; }
+    }
+}
